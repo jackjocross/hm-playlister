9d583eea109a008e8be7236479f1dff6e455cc99
jcros8 <jjcross@umich.edu>
2017-04-22 10:34:47 -0400

    cleaned up promise logic

--- src/index.js
+++ src/index.js
@@ -3,11 +3,9 @@
 import request from 'request';
 import fetch from 'isomorphic-fetch';
 import {argv} from 'yargs';
-
 import secrets from '../secrets.json';
 
 const {client_id, client_secret, refresh_token} = secrets;
-let {access_token} = secrets;
 
 const authOptions = {
   url: 'https://accounts.spotify.com/api/token',
@@ -22,97 +20,78 @@ const authOptions = {
   json: true,
 };
 
-request.post(authOptions, (error, response, body) => {
-  access_token = body.access_token;
-
+request.post(authOptions, (error, response, {access_token}) => {
   const options = {
     headers: {Authorization: 'Bearer ' + access_token},
   };
 
-  const profilePending = fetch('https://api.spotify.com/v1/me', options);
-  const playlistsPending = fetch(
+  const profileReq = fetch('https://api.spotify.com/v1/me', options).then(res =>
+    res.json(),
+  );
+  const playlistsReq = fetch(
     'https://api.spotify.com/v1/me/playlists',
     options,
-  );
+  ).then(res => res.json());
 
-  Promise.all([
-    profilePending,
-    playlistsPending,
-  ]).then(([profileReq, playlistsReq]) => {
-    Promise.all([
-      profileReq.json(),
-      playlistsReq.json(),
-    ]).then(([profile, playlists]) => {
-      const playlist = playlists.items.filter(item => item.name === argv.name)[
-        0
-      ];
-      if (playlist) {
-        populate(playlist, profile, options);
-      } else {
-        const createOpts = Object.assign({}, options, {
-          method: 'POST',
-          body: JSON.stringify({
-            name: argv.name,
-          }),
+  Promise.all([profileReq, playlistsReq]).then(([profile, playlists]) => {
+    const playlist = playlists.items.filter(item => item.name === argv.name)[0];
+    if (playlist) {
+      populate(playlist.id, profile.id, options);
+    } else {
+      const createOpts = Object.assign({}, options, {
+        method: 'POST',
+        body: JSON.stringify({
+          name: argv.name,
+        }),
+      });
+      fetch(
+        `https://api.spotify.com/v1/users/${profile.id}/playlists`,
+        createOpts,
+      )
+        .then(response => {
+          return response.json();
+        })
+        .then(newPlaylist => {
+          populate(newPlaylist.id, profile.id, options);
         });
-        fetch(
-          `https://api.spotify.com/v1/users/${profile.id}/playlists`,
-          createOpts,
-        )
-          .then(response => {
-            return response.json();
-          })
-          .then(newPlaylist => {
-            populate(newPlaylist, profile, options);
-          });
-      }
-    });
+    }
   });
 });
 
-function populate(playlist, profile, options) {
-  fetch(
-    `https://api.hypem.com/v2/popular?mode=${argv.mode}&count=50`,
-  ).then(body => {
-    body.json().then(json => {
-      const searchesPending = json.map(track => {
+function populate(playlistId, profileId, options) {
+  fetch(`https://api.hypem.com/v2/popular?mode=${argv.mode}&count=50`)
+    .then(res => res.json())
+    .then(tracks => {
+      const sparseUris = tracks.map(track => {
         const query = encodeURIComponent(`${track.artist} ${track.title}`);
 
         return fetch(
           `https://api.spotify.com/v1/search?q=${query}&type=track&limit=1`,
           options,
-        );
+        )
+          .then(res => res.json())
+          .then(
+            res =>
+              (res.tracks && res.tracks.total
+                ? fetch(res.tracks.items[0].href, options).then(res =>
+                    res.json(),
+                  )
+                : Promise.resolve({})),
+          )
+          .then(res => Promise.resolve(res.uri));
       });
 
-      Promise.all(searchesPending).then(searches => {
-        const resultsPending = searches.map(search => search.json());
-
-        Promise.all(resultsPending).then(results => {
-          const tracksPending = results
-            .filter(result => result.tracks && result.tracks.total)
-            .map(result => fetch(result.tracks.items[0].href, options));
-
-          Promise.all(tracksPending).then(tracks => {
-            const matchesPending = tracks.map(track => track.json());
-
-            Promise.all(matchesPending).then(matches => {
-              const uris = matches.map(match => match.uri);
-
-              const addOpts = Object.assign({}, options, {
-                method: 'PUT',
-                body: JSON.stringify({
-                  uris,
-                }),
-              });
-
-              fetch(
-                `https://api.spotify.com/v1/users/${profile.id}/playlists/${playlist.id}/tracks`,
-                addOpts,
-              );
-            });
-          });
-        });
-      });
+      Promise.all(sparseUris).then(sparseUris =>
+        fetch(
+          `https://api.spotify.com/v1/users/${profileId}/playlists/${playlistId}/tracks`,
+          {
+            ...options,
+            method: 'PUT',
+            body: JSON.stringify({
+              uris: sparseUris.filter(uri => uri),
+            }),
+          },
+        ),
+      );
     });
-  });
 }
